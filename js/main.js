c65f31c8823de0c0c782c17447930e09e9aebb66
C4ptainCrunch <nikita.marchant@gmail.com>
2013-03-17 00:49:56 +0100

    slight design changes + add pamela status in aside

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$(function(){
+    $("#slides").slides({
+        play: 3000,
+        hoverPause: true,
+        pause: 1,
+        paginationClass: 'pagination'
+    });
 
+    $.getJSON("http://api.urlab.be/spaceapi/status", function(json) {
+        if (json.state == "open")
+            statustext = "Nous sommes ouverts, viendez ! :-)";
+        else
+            statustext = "Le hackerspace est fermé pour l'instant";
+        $('aside').prepend($.parseHTML('<a href="http://pamela.urlab.be/" class="status status'+json.state+'">'+statustext+'</a>'));
+    });
+});
