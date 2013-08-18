d314773427d60fbee7cdac48d42226ac2b397170
Nikita Marchant <nikita.marchant@gmail.com>
2013-08-18 21:16:44 +0200

    Send a 301 when requesting /favicon.ico Redirect to the static dir

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import patterns, include, url
 
 import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'homepage.views.home', name='home'),
+    url(r'^favicon.ico$', RedirectView.as_view(url=settings.STATIC_URL+'img/favicon.ico')),
     # url(r'^homepage/', include('homepage.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
